aa0ca4f29d3ccd67b40ab25b323fe08101dfe63c
pelayord76 <pelayord76@educastur.es>
2024-02-07 10:13:50 +0100

    version final

--- gifexpertapp/src/services/getGifs.js
+++ gifexpertapp/src/services/getGifs.js
@@ -1,8 +1,9 @@
 export const getGifs = async (category) => {
-  const url = `https://api.giphy.com/v1/gifs/search?api_key=i4qRSpqKS25nyfD1ow95a4rIcRKs5kM4&q=${category}s&limit=10`;
+  //const url = `https://api.giphy.com/v1/gifs/search?api_key=i4qRSpqKS25nyfD1ow95a4rIcRKs5kM4&q=${category}s&limit=10`;
+  const url = 'http://localhost:3000/data';
   //fetch es una función para buscar
   const respuesta = await fetch(url);
-  const { data } = await respuesta.json();
+  const data = await respuesta.json();
 
 
   const gifs = data.map(imagen => (
